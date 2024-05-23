42d733da933dab520da6fef1b0c58b1a800de48c
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-23 16:45:11 -0300

    incluido comentário

--- CaixaEletronico/src/CaixaEletronico.java
+++ CaixaEletronico/src/CaixaEletronico.java
@@ -2,7 +2,7 @@ public class CaixaEletronico {
     public static void main(String[] args) {
 
         double saldo = 25.0;
-        double valorSolicitado = 26.00;
+        double valorSolicitado = 17.00;
 
         if (valorSolicitado < saldo) { 
             saldo = saldo - valorSolicitado;
@@ -10,6 +10,6 @@ public class CaixaEletronico {
         } else {
             System.out.println("Saldo insuficiente."); 
         }
-
+// controle de fluxo simples
     }
 }

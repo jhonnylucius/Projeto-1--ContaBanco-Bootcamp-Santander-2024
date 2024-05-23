3f7ea7e58998b3e29d842b04dba4a06474df1478
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-23 17:44:54 -0300

    condição ternária

--- ResultadoEscolar/src/ResultadoEscolar.java
+++ ResultadoEscolar/src/ResultadoEscolar.java
@@ -1,5 +1,5 @@
 public class ResultadoEscolar {
-    public static void main(String[] args) throws Exception {
+    /*public static void main(String[] args) throws Exception {
        int nota = 6;
 
        if (nota >= 7) 
@@ -8,7 +8,14 @@ public class ResultadoEscolar {
         System.out.println("Prova de recuperação");
        
        else 
-        System.out.println("Reprovado");         
-       }
+        System.out.println("Reprovado");*/
+
+        //condição ternária abaixo e acima if, else if e else esta como comentário
+        public static void main(String[] args) {
+            int nota = 6;
+            String resultado = nota >= 7 ? "Aprovado" : (nota >= 5 && nota < 7 ? "Recuperação" : "reprovado");
+            System.out.println(resultado);
+        }
+    }
     
-}
+

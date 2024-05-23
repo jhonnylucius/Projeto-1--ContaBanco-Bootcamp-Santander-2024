ee88a97ebdc45ff3d46df2eb4181a1e007f49bbd
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-23 16:53:26 -0300

    Projeto Resultado Escolar, patricando controle de fluxo composto

---
+++ ResultadoEscolar/src/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) throws Exception {
+       int nota = 6;
+
+       if (nota > 6) {
+        System.out.println("Reprovado");
+       else 
+        System.out.println("Aprovadp");         
+       }
+    }
+}

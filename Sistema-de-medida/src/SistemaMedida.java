cdbc6a887017bb29b6d0362b54b606f4de145ce9
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-23 18:09:01 -0300

    Sistema de medida - Switch Case - Estrutura de condições com java

---
+++ Sistema-de-medida/src/SistemaMedida.java
@@ -0,0 +1,17 @@
+public class SistemaMedida {
+    public static void main(String[] args) throws Exception {
+       String sigla = "M";
+
+       if (sigla == "P") 
+        System.out.println("Pequeno");
+            else if (sigla =="M") 
+                System.out.println("Médio"); 
+            else if (sigla =="M") 
+                System.out.println("Médio");
+            else 
+                System.out.println("Grande");   
+        
+        
+       
+    }
+}

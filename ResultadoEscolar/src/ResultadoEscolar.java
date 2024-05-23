bafa987c3adec609ab85c7aafef7489632420fc4
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-23 17:26:33 -0300

    acrestentando mais 1 fluxo

--- ResultadoEscolar/src/ResultadoEscolar.java
+++ ResultadoEscolar/src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) throws Exception {
-       int nota = 5;
+       int nota = 6;
 
-       if (nota >= 6) 
+       if (nota >= 7) 
         System.out.println("Aprovado");
+       else if (nota >=5 && nota <7) 
+        System.out.println("Prova de recuperação");
+       
        else 
         System.out.println("Reprovado");         
        }

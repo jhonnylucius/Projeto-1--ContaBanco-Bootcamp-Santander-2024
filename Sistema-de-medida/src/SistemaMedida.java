a695e2906a5f87cb3f8c245bec03b39b3c6002ff
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-23 19:55:23 -0300

    Estruturas Switch e if, elase, else if

--- Sistema-de-medida/src/SistemaMedida.java
+++ Sistema-de-medida/src/SistemaMedida.java
@@ -1,17 +1,36 @@
 public class SistemaMedida {
     public static void main(String[] args) throws Exception {
-       String sigla = "M";
-
+       String sigla = "G";
+/* 
        if (sigla == "P") 
         System.out.println("Pequeno");
             else if (sigla =="M") 
                 System.out.println("Médio"); 
-            else if (sigla =="M") 
-                System.out.println("Médio");
+            else if (sigla =="G") 
+                System.out.println("Grande");
             else 
-                System.out.println("Grande");   
-        
-        
+                System.out.println("indefinido");   
+   */     
+
+   // acima estrutura de condição if, else e else if 
+
+   // abaixo estrutura Switch
+     switch (sigla) {
+        case "P":{
+            System.out.println("Pequeno");    
+               break;
+        }
+        case "M":{
+            System.out.println("Médio");    
+               break;
+        }
+        case "G":{
+            System.out.println("Grande");    
+               break;
+        }
+        default:
+            break;
+     }   
        
     }
 }

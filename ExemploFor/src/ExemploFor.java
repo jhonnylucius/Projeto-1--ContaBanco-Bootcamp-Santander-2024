fd87a471c864386176017eae46846355b15c6426
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-25 16:51:16 -0300

    Exemplo For (estrutura de repetição - loops)

---
+++ ExemploFor/src/ExemploFor.java
@@ -0,0 +1,13 @@
+public class ExemploFor {
+
+    private static final String diamantes = null;
+
+    public static void main(String[] args) {
+        for (int diamantes = 1; diamantes <=20; diamantes ++ ) {
+            System.out.println("Contando diamantes " + diamantes); 
+
+    }
+
+            System.out.println("Estou rico....ihhuulll");
+}
+}

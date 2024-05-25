487094ddf862e578f5852f5e3161311ba3588047
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-05-24 22:14:55 -0300

    mesmo codigo usando switch case e as mesmas melhorias

--- PlanoOperadora/src/PlanoOperadora.java
+++ PlanoOperadora/src/PlanoOperadora.java
@@ -1,4 +1,4 @@
-public class PlanoOperadora {
+/*public class PlanoOperadora {
     public static void main(String[] args) {
         String plano = "m"; // Teste com "m" para verificar a insensibilidade à caixa
 
@@ -14,10 +14,38 @@ public class PlanoOperadora {
         } else {
             System.out.println("Plano inválido. Por favor, escolha entre B, M ou T.");
         }
-    }
-}
+    }*/
+
+
+
 /*aperfeiçoado com tratamento de exceções, 
 e deixando o codigo ler qualquer tipo de letra, 
-maiusculas ou minusculas com equalsIgnoreCase para cada plano.
+maiusculas ou minusculas com equalsIgnoreCase para cada plano.*/
+
+// abaixo mesmo codigo com as mesmas melhorias com switch case
+
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "t"; // Teste com "m" para verificar a insensibilidade à caixa
+
+        switch (plano.toUpperCase()) { // Convertemos para caixa alta para a comparação
+            case "B":
+                System.out.println("100 minutos de ligação");
+                break;
+            case "M":
+                System.out.println("100 minutos de ligação");
+                System.out.println("Whats e instagram grátis");
+                break;
+            case "T":
+                System.out.println("100 minutos de ligação");
+                System.out.println("Whats e instagram grátis");
+                System.out.println("5GB YouTube");
+                break;
+            default:
+                System.out.println("Plano inválido. Por favor, escolha entre B, M ou T.");
+        }
+    }
+}
+
 
 

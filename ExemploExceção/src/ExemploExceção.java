736c0358d759be5d462dd94e4b6b4dbff6d0b69b
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-07 11:45:27 -0300

    Trantando a excenção de maneira mais adequada

--- ExemploExceção/src/ExemploExceção.java
+++ ExemploExceção/src/ExemploExceção.java
@@ -1,4 +1,4 @@
-
+/* 
 import java.text.NumberFormat;
 import java.text.ParseException;
 
@@ -14,5 +14,24 @@ public static void main(String[] args) throws ParseException {
         e.printStackTrace();
 }
  }
+} */
+// retornando apenas as informações da exceçãp no terminal
+// abaixo, trantando essa essão para que retorne a msg dizendo o pq do erro e onde ele está, caso o valor não seja adequado 
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExemploExceção {
+
+    public static void main(String[] args) {
+        Number valor = null;
+        try {
+            valor = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(valor);
+        } catch (ParseException e) {
+            System.err.println("Erro ao converter a string para número: " + e.getMessage());
+            // Aqui você pode definir valor = null ou lançar outra exceção.
+        }
+    }
 }
+
 

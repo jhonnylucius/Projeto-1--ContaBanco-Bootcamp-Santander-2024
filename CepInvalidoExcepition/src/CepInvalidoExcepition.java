f9d25756a750206aae4d382f061b56006d729d60
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-07 13:57:47 -0300

    ATUALIZAÇÃO

--- CepInvalidoExcepition/src/CepInvalidoExcepition.java
+++
@@ -1,6 +0,0 @@
-public class CepInvalidoExcepition extends Exception {
-    
-    }
-    
-    
-
--- CepInvalidoExcepition/src/FormatadorCepExemplo.java
+++ CepInvalidoExcepition/src/FormatadorCepExemplo.java
@@ -1,22 +1,18 @@
-import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 public class FormatadorCepExemplo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CepInvalidoExcepition {
         Scanner inputScanner = new Scanner(System.in);
         try {
             while (true) {
                 System.out.println("Digite seu CEP, por favor!");
                 String cepDigitado = inputScanner.nextLine();
 
-                try {
-                    String cepFormatado = formatarCep(cepDigitado);
-                    System.out.println(cepFormatado);
-                    break; // Exit the loop if the CEP is valid
-                } catch (CepInvalidoExcepition e) {
-                    System.err.println("CEP inválido! " + e.getMessage() + " Por favor, tente novamente.");
-                }
+                String cepFormatado = formatarCep(cepDigitado);
+                System.out.println(cepFormatado);
+                break; // Exit the loop if the CEP is valid
             }
         } finally {
             inputScanner.close();
@@ -25,17 +21,27 @@ public class FormatadorCepExemplo {
 
     static String formatarCep(String cep) throws CepInvalidoExcepition {
         if (cep.length() != 8) {
-            throw new CepInvalidoExcepition("O CEP deve ter 8 dígitos.");
+            try {
+                throw new CepInvalidoException();
+            } catch (CepInvalidoException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         if (!cep.matches("\\d+")) { // Verificação para letras
-            throw new CepInvalidoExcepition("O CEP deve conter apenas números.");
+            try {
+                throw new CepInvalidoException();
+            } catch (CepInvalidoException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         return String.format("Seu CEP é: %s-%s", cep.substring(0, 5), cep.substring(5));
     }
 }
 
 class CepInvalidoExcepition extends Exception {
-    public CepInvalidoExcepition(String message) {
+    public CepInvalidoException(String message) {
         super(message);
     }
 }

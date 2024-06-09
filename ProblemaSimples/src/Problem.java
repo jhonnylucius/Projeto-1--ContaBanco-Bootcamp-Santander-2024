fbcd0ce9e72ea3e38e0465f8dc7b948600cd3ba1
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-09 14:08:26 -0300

    Problema simples

---
+++ ProblemaSimples/src/Problem.java
@@ -0,0 +1,21 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.Scanner;
+
+public class Problem {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a, b, prod;
+		System.out.println("Digite o primeiro número:");
+		a = scanner.nextInt();
+		System.out.println("Digite o primeiro número:");
+		b = scanner.nextInt();
+		 //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+		prod = a*b;  
+
+		System.out.println("PROD = " + prod);
+		scanner.close();25
+		
+	}
+	
+}

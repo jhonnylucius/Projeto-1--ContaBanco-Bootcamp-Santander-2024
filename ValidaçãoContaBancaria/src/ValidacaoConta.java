6fe464210676381aa98ec039d9a3573bc02643ba
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-09 12:02:09 -0300

    atualização

---
+++ ValidaçãoContaBancaria/src/ValidacaoConta.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ValidacaoConta {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       String numeroConta = scanner.nextLine();
+
+        try {
+            // Verifica se o número da conta tem 8 dígitos
+            if (numeroConta.length() != 8) {
+                throw new IllegalArgumentException("Erro: Numero de conta invalido. Digite exatamente 8 digitos.");
+            }
+
+            // Se o número da conta é válido
+            System.out.println("Numero de conta valido.");
+
+        } catch (IllegalArgumentException e) {
+            // Exibe a mensagem de erro
+            System.out.println(e.getMessage());
+        }
+    }
+}

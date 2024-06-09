e2fe562e1a7d55c03c6b0553edc71e4619e6c96c
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-09 12:17:31 -0300

    colocando a opção do usuário digitar

--- ValidaçãoContaBancaria/src/VerificadorNumeroConta.java
+++ ValidaçãoContaBancaria/src/VerificadorNumeroConta.java
@@ -4,7 +4,7 @@ public class VerificadorNumeroConta {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Digite numero da conta: ");
        String numeroConta = scanner.nextLine();
 
         try {

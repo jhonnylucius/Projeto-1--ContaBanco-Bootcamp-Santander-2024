a458dc20a011aa37e66a31fb9b0e4ed3b74c56ce
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-12 13:24:01 -0300

    finalizado

--- Autodromo/src/Autodromo.java
+++ Autodromo/src/Autodromo.java
@@ -1,20 +1,17 @@
-import Veiculo;
-package autodromo;
-import autodromo.veiculo.Moto;
-
-public class Autodromo extends Veiculo{
+public class Autodromo {
     public static void main(String[] args) {
         Carro jeep = new Carro();
         jeep.setChassi("123456");
-        //jeep.ligar();
+  //jeepligar()
 
         Moto z400 = new Moto();
         z400.setChassi("654321");
         //z400.ligar();
 
-        Veiculo coringa = jeep;
+        final Veiculo coringa = z400;
         coringa.ligar();
+
     }
 }
- 
+
 
--- Autodromo/src/Moto.java
+++ Autodromo/src/Moto.java
@@ -1,6 +1,4 @@
-import Veiculo;
 
-package autodromo.veiculo;
 public class Moto extends Veiculo {
    public void ligar (){
     System.out.println("MOTO LIGADA");
